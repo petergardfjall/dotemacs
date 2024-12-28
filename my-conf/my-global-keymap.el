171f12ad63b7071ed689f3cda831e898840072c7
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2024-12-28 11:25:36 +0100

    refactor my-global-keymap.el

--- my-conf/my-global-keymap.el
+++ my-conf/my-global-keymap.el
@@ -1,4 +1,4 @@
-;;; package --- Assorted buffer-related functions.  -*- lexical-binding: t -*-
+;;; package --- Global keymap definitions.  -*- lexical-binding: t -*-
 ;;;
 ;;; Commentary:
 ;;;
@@ -6,28 +6,6 @@
 ;;;
 
 (message "loading %s ..." load-file-name)
-
-(defun my-keyboard-quit-dwim ()
-  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
-The generic `keyboard-quit' does not do the expected thing when the
-minibuffer is open: we want it to close the minibuffer, even without
-explicitly focusing it.  The DWIM behaviour of this command is as
-follows:
-- When the region is active, disable it.
-- When a minibuffer is open, but not focused, close the minibuffer.
-- When the Completions buffer is selected, close it.
-- In every other case use the regular `keyboard-quit'."
-  (interactive)
-  (cond
-   ((region-active-p)
-    (keyboard-quit))
-   ((derived-mode-p 'completion-list-mode)
-    (delete-completion-window))
-   ((> (minibuffer-depth) 0)
-    (abort-recursive-edit))
-   (t
-    (keyboard-quit))))
-
 
 ;; Define keybindings that go into the the `global-map'.
 (let ((gm global-map))
@@ -51,6 +29,27 @@ follows:
   (define-key gm (kbd "C-x w <down>")  #'windmove-down)
   (define-key gm (kbd "C-x w <right>") #'windmove-right)
   (define-key gm (kbd "C-x w <left>")  #'windmove-left))
+
+(defun my-keyboard-quit-dwim ()
+  "Do-What-I-Mean behaviour for a general `keyboard-quit'.
+The generic `keyboard-quit' does not do the expected thing when the
+minibuffer is open: we want it to close the minibuffer, even without
+explicitly focusing it.  The DWIM behaviour of this command is as
+follows:
+- When the region is active, disable it.
+- When a minibuffer is open, but not focused, close the minibuffer.
+- When the Completions buffer is selected, close it.
+- In every other case use the regular `keyboard-quit'."
+  (interactive)
+  (cond
+   ((region-active-p)
+    (keyboard-quit))
+   ((derived-mode-p 'completion-list-mode)
+    (delete-completion-window))
+   ((> (minibuffer-depth) 0)
+    (abort-recursive-edit))
+   (t
+    (keyboard-quit))))
 
 (provide 'my-global-keymap)
 ;;; my-global-keymap.el ends here.
